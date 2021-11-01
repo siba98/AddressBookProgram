d1c4067a637351c1a2af356fcbdbfddd0f967827
siba98 <gyansiba.patro@gmail.com>
2021-11-01 15:46:22 +0530

    [Siba]:Added Implementation For Listing all Contact Present in the AddressBook

--- AddressBook_Collection_Assignments/AddressBookSystem.cs
+++ AddressBook_Collection_Assignments/AddressBookSystem.cs
@@ -1,18 +1,22 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace AddressBook2
 {
     public class AddressBookSystem
     {
+        //declaring a List
+        public List<Contacts> People = new List<Contacts>();
+
         /// <summary>
         /// UC1 ==> added contact details
         /// </summary>
-        private void ContactDetails()
+        public void ContactDetails()
         {
             //Created object of Contacts class
             Contacts person = new Contacts();
 
-            Console.WriteLine("***Enter the Person Details***");
+            Console.WriteLine("******Enter the Person Details******");
 
             Console.Write("Enter First Name: ");
             person.firstname = Console.ReadLine();
@@ -38,7 +42,18 @@ namespace AddressBook2
             Console.Write("Enter the Email ID: ");
             person.email = Console.ReadLine();
 
-            Console.WriteLine("*****************\n");
+            //added the details of the person to the list
+            People.Add(person);
+            Console.WriteLine("Successfully Added the Person Details");
+        }
+
+
+        /// <summary>
+        /// printing the details
+        /// </summary>
+        /// <param name="person"></param>
+        public void PrintDetails(Contacts person)
+        {
             Console.WriteLine("First Name: " + person.firstname);
             Console.WriteLine("Last Name: " + person.lastname);
             Console.WriteLine("Address: " + person.address);
@@ -52,6 +67,27 @@ namespace AddressBook2
 
 
         /// <summary>
+        /// UC2 ==> Check any details present in list or not 
+        /// </summary>
+        public void ListAllContacts()
+        {
+            if (People.Count == 0)
+            {
+                Console.WriteLine("Your address book is empty. Press any key to continue.");
+                Console.ReadKey();
+                return;
+            }
+            Console.WriteLine("Here are the current people in your address book:\n");
+            foreach (var person in People)
+            {
+                PrintDetails(person);
+            }
+            Console.WriteLine("\nPress any key to continue.");
+            Console.ReadKey();
+        }
+
+
+        /// <summary>
         /// Choose options for adding the details in a address book
         /// </summary>
         public void ChooseOption()
@@ -60,7 +96,7 @@ namespace AddressBook2
             bool exit = false;
             while (exit != true)
             {
-                Console.WriteLine("Choose a number: " + "\n1 :Create Contact\n" + "2 :Exit From the Address Book\n");
+                Console.WriteLine("Choose a number: " + "\n1 :Create Contact\n" + "2 :List All People Present in the List\n"+ "3 :Exit From the Address Book\n");
                 int options = Convert.ToInt32(Console.ReadLine());
                 switch (options)
                 {
@@ -68,6 +104,9 @@ namespace AddressBook2
                         ContactDetails();
                         break;
                     case 2:
+                        ListAllContacts();
+                        break;
+                    case 3:
                         exit = true;
                         break;
                     default:
